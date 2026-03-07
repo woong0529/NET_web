afe38bfdcdf1319f28fd77f42aa44174cebd0409
Woong <theregularwoong2@naver.com>
2026-03-07 16:46:21 +0900

    api key를 숨기지 않고 http 리퍼러 제한 걸기로 보안을 강화하기로

---
+++ api_key.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBRNJcRIChGLeaFdH7JShTM7_ZJnjP8R-w",
+    authDomain: "net-login-7c68a.firebaseapp.com",
+    projectId: "net-login-7c68a",
+    storageBucket: "net-login-7c68a.firebasestorage.app",
+    messagingSenderId: "53195102418",
+    appId: "1:53195102418:web:513dcde61a59895047350b"
+};
+
+export default firebaseConfig;
--- api_key_ex.js
+++
@@ -1,5 +0,0 @@
-const firebaseConfig = {
-  apiKey: "여기에 본인의 API 키를 넣으세요",
-  authDomain: "여기에 프로젝트 도메인을 넣으세요"
-};
-export default firebaseConfig;
